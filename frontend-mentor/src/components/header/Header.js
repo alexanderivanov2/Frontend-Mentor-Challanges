4518e35f0a660d06b29c564976454fa2aec04677
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-04 21:57:55 +0300

    refactor

--- frontend-mentor/src/components/header/Header.js
+++ frontend-mentor/src/components/header/Header.js
@@ -14,12 +14,12 @@ function Header({screenWidth}) {
     
     return (
         <> 
-            {device == 'desktop'
+            {device == 'mobile'
              &&
              <header className={styles.headerNavigation}>
              <section className={styles.headerNavigation__box}>
                  <article className={styles.headerNavigation__titleBox}>
-                     <h1>Frontend Mentor Challanges</h1>
+                     <h2>Frontend Mentor Challanges</h2>
                  </article>
                  <nav className={styles.headerNavigation__navigationBox}>
                      <ul className={styles.headerNavigation__navigation}>
