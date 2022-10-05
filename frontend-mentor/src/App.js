faa4d50514b8f8d4dc92ec98bbf7270e0e6d14b7
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-05 22:28:09 +0300

    refactor navbar

--- frontend-mentor/src/App.js
+++ frontend-mentor/src/App.js
@@ -8,15 +8,20 @@ import ProductPreviewCard from "./components/newbieSection/productPreviewCard/Pr
 function App() {
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
     
-    window.addEventListener('resize', (e) => {
-        setScreenWidth(window.innerWidth);
-    });
+    useEffect(() => {
+        window.addEventListener('resize', (e) => {
+            setScreenWidth(window.innerWidth);
+        });
+
+        return window.removeEventListener('resize', this);
+    }, [window.innerWidth]);
+
 
     return (
         <BrowserRouter>
             <Header screenWidth={screenWidth}/>
             <Routes>
-                <Route path="/"/>
+                <Route path="/" element={<NewbieSection screenWidth={screenWidth}/>}/>
                 <Route path='/newbie' element={<NewbieSection screenWidth={screenWidth}/>}/>
                 <Route path='/newbie/project-one' element={<ProductPreviewCard screenWidth={screenWidth}/>}/>
             </Routes>
--- frontend-mentor/src/components/header/Header.js
+++ frontend-mentor/src/components/header/Header.js
@@ -15,35 +15,35 @@ function Header({screenWidth}) {
     }, [screenWidth])
     
     const onBurgerMenuClick = (e) => {
+        console.log(window.screen.width);
         const result = menuOpen ? false : true;
         setMenuOpen(result);
     }
     return (
         <>
             <header className={styles.headerNavigation}>
+                <section className={styles.navigationBar}>
+                    <article className={styles.navigationBar__titleBox}>
+                        <h2>{device === 'mobile' ? 'FMC' : 'Frontend Mentor Challanges'}</h2>
+                    </article>
+        
+                    <div className={styles.navigationBar__burger} onClick={onBurgerMenuClick}>
+                        <div className={styles.navigationBar__burger__line}></div>
+                    </div> 
+                </section>
+  
                 {menuOpen 
-                    ? 
-                        <section className={styles.headerNavigation__box}>
-                            <nav className={styles.headerNavigation__navigationBox}>
-                                <ul className={styles.headerNavigation__navigation}>
-                                    <li className={styles.headerNavigation__navigation__item}><Link to='newbie'>Newbie</Link></li>
-                                    <li className={styles.headerNavigation__navigation__item}><Link to='junior'>Junior</Link></li>
-                                    <li className={styles.headerNavigation__navigation__item}>Intermediate</li>
-                                    <li className={styles.headerNavigation__navigation__item}>Advanced</li>
-                                    <li className={styles.headerNavigation__navigation__item}>Guru</li>
-                                </ul>
-                            </nav>
-                        </section>
-                    :
-                    <>
-                        <article className={styles.headerNavigation__titleBox}>
-                            <h2>{device === 'mobile' ? 'FMC' : 'Frontend Mentor Challanges'}</h2>
-                        </article>
-                    </>
-                }
-                <div className={styles.headerNavigation__burger} onClick={onBurgerMenuClick}>
-                    <span className={styles.headerNavigation__burger__line}></span>
-                </div>    
+                    &&                         
+                    <nav className={styles.headerNavigation__navigationBox}>
+                        <ul className={styles.headerNavigation__navigationBox__navigation}>
+                            <li className={styles.headerNavigation__navigationBox__item}><Link to='newbie'>Newbie</Link></li>
+                            <li className={styles.headerNavigation__navigationBox__item}><Link to='junior'>Junior</Link></li>
+                            <li className={styles.headerNavigation__navigationBox__item}><Link to='intermediate'>Intermediate</Link></li>
+                            <li className={styles.headerNavigation__navigationBox__item}><Link to='advanced'>Advanced</Link></li>
+                            <li className={styles.headerNavigation__navigationBox__item}><Link to='guru'>Guru</Link></li>
+                        </ul>
+                    </nav>            
+                } 
          </header> 
         </>
     );
