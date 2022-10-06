63108ad0a7a2c45179e718b9d692e15d1c815db0
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-06 16:12:40 +0300

    newbie section mobile view

--- frontend-mentor/src/components/newbieSection/NewbieSection.js
+++ frontend-mentor/src/components/newbieSection/NewbieSection.js
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from './NewbieSection.module.scss';
+import productPreviewCardIMG from './img/productPreviewCard.png';
 
 function NewbieSection() {
     return (
         <>
             <section className={styles.newbieSection}>
-                    <article>
-                        <h2>Title</h2>
-                        <p>Short Description</p>
-                        <Link to='/newbie/project-one'>Go To Project</Link>
+                    <article className={styles.newbieSection__project}>
+                        <div className={styles.newbieSection__project__imgDiv}>
+                            <img src={productPreviewCardIMG} alt="product preview card"/>
+                        </div>
+                        <h2 className={styles.newbieSection__project__title}>Product Preview Card</h2>
+
+                        <Link to='/newbie/project-one'
+                        className={styles.newbieSection__project__link}>Go To Project</Link>
                     </article>
             </section>
             
