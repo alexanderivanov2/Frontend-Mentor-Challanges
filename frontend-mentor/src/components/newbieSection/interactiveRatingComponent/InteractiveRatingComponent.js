6d53a7fae9089f40fe2a58ea2dae3ea898919c06
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-08 12:28:33 +0300

    create useState for vote and eventHandlers for select rating value and submit rating

--- frontend-mentor/src/components/newbieSection/interactiveRatingComponent/InteractiveRatingComponent.js
+++ frontend-mentor/src/components/newbieSection/interactiveRatingComponent/InteractiveRatingComponent.js
@@ -1,6 +1,38 @@
+import {useState} from 'react';
 function InteractiveRatingComponent() {
+    const [vote, setVote] = useState({
+        isVote: false,
+        voteValue: 0,
+    });
+
+    const onClickRatingValue = (e) => {
+        const value = e.target.voteValue;
+        console.log(value);
+        setVote(state => ({
+            ...state,
+            voteValue: value,
+        }));
+    }
+
+    const onSubmitRatignVote = (e) => {
+        if (vote.voteValue > 0) {
+            setVote(state => ({
+                ...state,
+                isVote: true,
+            }));
+        }
+    }
+
     return (
         <>
+            {vote.isVote 
+                ?
+                    <>
+                        
+                    </>
+                : 
+                    ''
+            }
         </>
     );
 }
