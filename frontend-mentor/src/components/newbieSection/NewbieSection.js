1619931e208674d774b76c4ab435d6ac97a1fd97
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-10-22 19:01:11 +0300

    add image and link to order summary newbie section

--- frontend-mentor/src/components/newbieSection/NewbieSection.js
+++ frontend-mentor/src/components/newbieSection/NewbieSection.js
@@ -4,10 +4,11 @@ import productPreviewCardIMG from './img/productPreviewCard.png';
 import qrCodeComponent from './img/qrCodeComponent.png';
 import interactiveRatingIMG from './img/interactive-rating-component.png';
 import nftPreviewCardImg from './img/nft-preview-card.png';
+import orderSummaryImg from "./img/orderSummary.png";
 import ChallangeCardComponent from "./ChallangeCardComponent";
 
 const challanges = [
-    {title: "Order Summary Component", linkTo: "/newbie/order-summary-component", img: nftPreviewCardImg},
+    {title: "Order Summary Component", linkTo: "/newbie/order-summary-component", img: orderSummaryImg},
     {title: "NFT Preview Card", linkTo: "/newbie/nft-preview-card", img: nftPreviewCardImg },
     {title: "Interactive Ratign Component", linkTo: "/newbie/interactive-rating-component", img: interactiveRatingIMG },
     {title: "QR Code Component", linkTo: "/newbie/qr-code-component", img: qrCodeComponent },
