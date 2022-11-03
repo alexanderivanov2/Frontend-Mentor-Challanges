5f9bede54b776f6d304ac12991e06949d0d9e87e
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-11-03 14:46:50 +0200

    create html skeleton

--- frontend-mentor/src/components/newbieSection/statsPreviewCard/StatsPreviewCard.js
+++ frontend-mentor/src/components/newbieSection/statsPreviewCard/StatsPreviewCard.js
@@ -1,10 +1,30 @@
 import styles from './StatsPreviewCard.module.scss';
-
+import imgMobile from './img/image-header-mobile.jpg';
+import imgDesktop from './img/image-header-desktop.jpg';
 
 function StatsPreviewCard() {
     return (
-        <>
-        </>
+        <section className={styles.previewCardPage}>
+            <article className={styles.previewCard}>
+                {window.screen.width < 750 
+                    ? 
+                        <img className={styles.previewCard__img} src={imgMobile} alt='stats preview card Mobile'/>
+                    :
+                        <img className={styles.previewCard__img} src={imgDesktop} alt='stats preview card Desktop'/>
+                }
+
+                <h2 className={styles.previewCard__header}>Get <span>insights</span> that help your business grow.</h2>
+                <p className={styles.previewCard__description}>
+                    Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+                    experience, and overall efficiency.
+                </p>
+                <ul className={styles.previewCard__stats_list}>
+                    <li><p>10k+</p><p>companies</p></li>
+                    <li><p>314</p><p>templates</p></li>
+                    <li><p>12m+</p><p>queries</p></li>
+                </ul>
+            </article>
+        </section>
     );
 }
 
